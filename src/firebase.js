46860909cad6f556a142a710256c1cc164222ad9
Sebastian Hurtado <sebassdc@gmail.com>
2020-02-22 11:54:06 -0500

    Prettier and adding file changes

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,15 @@ import * as firebase from 'firebase'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5jffTgh4NByzZxwMzgVgNCV634fwrBz0",
-  authDomain: "multitenancy-siigo.firebaseapp.com",
-  databaseURL: "https://multitenancy-siigo.firebaseio.com",
-  projectId: "multitenancy-siigo",
-  storageBucket: "multitenancy-siigo.appspot.com",
-  messagingSenderId: "154212073064",
-  appId: "1:154212073064:web:4c64bb7722ed12086185dd",
-  measurementId: "G-405YC10KR1"
-};
+  apiKey: 'AIzaSyA5jffTgh4NByzZxwMzgVgNCV634fwrBz0',
+  authDomain: 'multitenancy-siigo.firebaseapp.com',
+  databaseURL: 'https://multitenancy-siigo.firebaseio.com',
+  projectId: 'multitenancy-siigo',
+  storageBucket: 'multitenancy-siigo.appspot.com',
+  messagingSenderId: '154212073064',
+  appId: '1:154212073064:web:4c64bb7722ed12086185dd',
+  measurementId: 'G-405YC10KR1',
+}
 
 firebase.initializeApp(firebaseConfig)
 export const databaseRef = firebase.database().ref()
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,4 @@
-import { combineReducers } from "redux";
-import { reducer as auth } from "./auth";
+import { combineReducers } from 'redux'
+import { reducer as auth } from './auth'
 
-export default combineReducers({ auth });
+export default combineReducers({ auth })
