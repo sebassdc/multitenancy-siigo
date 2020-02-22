c2a149b84b8b17a4c5babcb5e7ef9918ffcd3db7
Sebastian Hurtado <sebassdc@gmail.com>
2020-02-22 16:11:12 -0500

    Finishing Login routing

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css'
 import Welcome from './views/Welcome'
 import SignUp from './views/SignUp'
-// import Login from './views/Login'
+import Login from './views/Login'
 
 const App = () => {
   return (
     <Switch>
       <Route path="/" exact component={Welcome} />
       <Route path="/signup" component={SignUp} />
-      {/* <Route path="/" component={Login} /> */}
+      <Route path="/login" component={Login} />
       <SignUp />
     </Switch>
   )
